359ae0b1d1b960ed1710675dd9f9f0746d5f7121
DaniloPaixaoJesus <contato.danilo.paixao@gmail.com>
2019-11-06 10:59:24 -0300

    refactory to clean code

--- src/main/java/br/com/danilopaixao/driver/resource/DriverResource.java
+++ src/main/java/br/com/danilopaixao/driver/resource/DriverResource.java
@@ -1,16 +1,16 @@
 package br.com.danilopaixao.driver.resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import br.com.danilopaixao.driver.model.Driver;
 import br.com.danilopaixao.driver.model.DriverList;
@@ -21,41 +21,30 @@ import br.com.danilopaixao.driver.service.DriverService;
 @RequestMapping("/api/v1/drivers")
 public class DriverResource {
 	
+	private static final Logger logger = LoggerFactory.getLogger(DriverResource.class);
+	
 	@Autowired
 	private DriverService service;
 	
-	@Autowired
-	private RestTemplate restTemplate;
-	
-	@RequestMapping(value = "/init", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@GetMapping(value = "/init", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseStatus(HttpStatus.OK)
 	public DriverList insertDriver() {
 		return service.init();
 	}
 	
 	
-	@RequestMapping(value = "", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseStatus(HttpStatus.OK)
 	public DriverList getAllDriver() {
+		logger.info("##DriverResource#getAllDriver: no argument");
 		return service.getAllDriver();
 	}
 	
-	@RequestMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	@ResponseStatus(HttpStatus.OK)
 	public @ResponseBody Driver getDriver(@PathVariable("id") final String id) {
+		logger.info("##DriverResource#getDriver({})", id);
 		return service.getDriver(id); 
 	}
 	
-	@HystrixCommand(fallbackMethod ="getFallback")
-	@RequestMapping(value = "/callbackteste/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-	@ResponseStatus(HttpStatus.OK)
-	public @ResponseBody Driver getTesteFallback(@PathVariable("id") final String id) {
-		Driver driver = restTemplate.getForObject("http://driver/teste/"+id, Driver.class);
-		return driver;
-	}
-	
-	public Driver getFallback(@PathVariable("id") final String id) {
-		return new Driver("NOT FOUND", "", "", "");
-	}
-
 }
--- src/main/java/br/com/danilopaixao/driver/service/DriverService.java
+++ src/main/java/br/com/danilopaixao/driver/service/DriverService.java
@@ -1,38 +1,43 @@
 package br.com.danilopaixao.driver.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.danilopaixao.driver.model.Driver;
 import br.com.danilopaixao.driver.model.DriverList;
 import br.com.danilopaixao.driver.repository.DriverRepository;
+import br.com.danilopaixao.driver.resource.DriverResource;
 
 @Service
 public class DriverService {
-
-//	@Autowired
-//	private RestTemplate restTemplate;
 	
+	private static final Logger logger = LoggerFactory.getLogger(DriverResource.class);
+
 	@Autowired
 	private DriverRepository driverRepository;
 	
 	public DriverList init() {
 		// in case of duplicate key, ignore and go ahead
-		try {insertDriver(new Driver("93418DF0R09QSDF", "Kalles Grustransporter", "AB", "Cementvägen 8, 111 11 Södertälje"));}catch (Exception e) {}
-		try {insertDriver(new Driver("623480520FDF2", "Johans Bulk", "AB", "Balkvägen 12, 222 22 Stockholm"));}catch (Exception e) {}
-		try {insertDriver(new Driver("7428DFEC8137652", "Haralds Värdetransporter", "AB", "Budgetvägen 1, 333 33 Uppsala"));}catch (Exception e) {}
+		try {insertNew(new Driver("93418DF0R09QSDF", "Kalles Grustransporter", "AB", "Cementvägen 8, 111 11 Södertälje"));}catch (Exception e) {}
+		try {insertNew(new Driver("623480520FDF2", "Johans Bulk", "AB", "Balkvägen 12, 222 22 Stockholm"));}catch (Exception e) {}
+		try {insertNew(new Driver("7428DFEC8137652", "Haralds Värdetransporter", "AB", "Budgetvägen 1, 333 33 Uppsala"));}catch (Exception e) {}
 		return this.getAllDriver();
 	}
 	
-	public Driver insertDriver(Driver driver) {
+	public Driver insertNew(Driver driver) {
+		logger.info("##DriverService#insertNew: {}", driver);
 		return driverRepository.insert(driver);
 	}
 	
 	public DriverList getAllDriver() {
+		logger.info("##DriverService#getAllDriver: nor arguments");
 		return new DriverList(driverRepository.findAll());
 	}
 	
 	public Driver getDriver(String id) {
+		logger.info("##DriverService#getDriver({})", id);
 		return driverRepository.findById(id).orElse(null);
 	}
 	
