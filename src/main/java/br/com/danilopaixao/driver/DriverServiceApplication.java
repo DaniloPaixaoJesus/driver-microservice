45bcc220e34dcd1cd0f30a4ed0ac1c5445516d8b
Danilo <contato.danilo.paixao@gmail.com>
2023-09-27 20:55:06 -0300

    upgrade spring to version 3 - refactories

--- src/main/java/br/com/danilopaixao/driver/DriverServiceApplication.java
+++ src/main/java/br/com/danilopaixao/driver/DriverServiceApplication.java
@@ -2,15 +2,15 @@ package br.com.danilopaixao.driver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableCircuitBreaker
+//@EnableDiscoveryClient
+//@EnableCircuitBreaker
 public class DriverServiceApplication {
 
     public static void main(String[] args) {
@@ -18,7 +18,7 @@ public class DriverServiceApplication {
     }
     
     @Bean
-	@LoadBalanced
+//	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	}
